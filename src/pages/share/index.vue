ffa506925a6f0460053265f3d4fe30a10d641258
lessiebmyers <lessiebmyers@gmail.com>
2025-03-14 16:35:00 +0800

    feat: 样式布局

--- src/pages/share/index.vue
+++ src/pages/share/index.vue
@@ -1,36 +1,48 @@
 <script setup lang="ts">
 const email = ref<string>('')
-
-const emailField = ref(null)
-
-// 失去焦点时触发校验
-function validateOnBlur() {
-  emailField.value.validate()
-}
-
-function validator(value: string) {
-  if (!value)
-    return true // 允许为空
-  const emailPattern = /^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i
-  return emailPattern.test(value)
-}
 </script>
 
 <template>
+  <van-row align="center">
+    <van-col span="12" class="h-100 bg-red text-center leading-100">
+      S
+    </van-col>
+    <van-col span="12" class="h-50 items-center bg-blue !flex">
+      <div class="w-full translate-x-[-10%] text-right">
+        QD<br>
+        QD
+      </div>
+    </van-col>
+  </van-row>
+  <van-row align="center">
+    <van-col span="8" class="h-100 bg-green text-center leading-100">
+      S
+    </van-col>
+    <van-col span="8" class="h-50 bg-bluegray text-center leading-50">
+      Q
+    </van-col>
+
+    <van-col span="8" class="h-50 bg-blue text-center leading-50">
+      <van-row class="h-25 leading-25">
+        <van-col span="24" class="bg-green">
+          D
+        </van-col>
+      </van-row>
+
+      <van-row class="h-25 leading-25">
+        <van-col span="12" class="bg-red">
+          d1
+        </van-col>
+        <van-col span="12" class="bg-coolGray">
+          d2
+        </van-col>
+      </van-row>
+    </van-col>
+  </van-row>
   <van-cell-group inset>
     <van-cell title="分享地址" value="内容1" />
     <van-cell title="分享地址" value="内容2" label="描述信息1" />
     <van-field v-model="email" type="email" placeholder="请输入邮箱" label="邮箱" required />
-    <van-field
-      ref="emailField"
-      v-model="email"
-      label="邮箱"
-      type="text"
-      placeholder="请输入邮箱"
-      required
-      :rules="[{ validator, message: '请输入正确邮箱' }]"
-      @blur="validateOnBlur"
-    />
   </van-cell-group>
 </template>
 
