93c14e6166150298de14e157610879f0f93cf8c8
lessiebmyers <lessiebmyers@gmail.com>
2025-03-14 11:41:16 +0800

    feat: email test

--- src/pages/share/index.vue
+++ src/pages/share/index.vue
@@ -1,7 +1,36 @@
+<script setup lang="ts">
+const email = ref<string>('')
+
+const emailField = ref(null)
+
+// 失去焦点时触发校验
+function validateOnBlur() {
+  emailField.value.validate()
+}
+
+function validator(value: string) {
+  if (!value)
+    return true // 允许为空
+  const emailPattern = /^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i
+  return emailPattern.test(value)
+}
+</script>
+
 <template>
   <van-cell-group inset>
     <van-cell title="分享地址" value="内容1" />
     <van-cell title="分享地址" value="内容2" label="描述信息1" />
+    <van-field v-model="email" type="email" placeholder="请输入邮箱" label="邮箱" required />
+    <van-field
+      ref="emailField"
+      v-model="email"
+      label="邮箱"
+      type="text"
+      placeholder="请输入邮箱"
+      required
+      :rules="[{ validator, message: '请输入正确邮箱' }]"
+      @blur="validateOnBlur"
+    />
   </van-cell-group>
 </template>
 
